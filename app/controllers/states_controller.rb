e487ba500b00d4c01eba931e72864e72408b0092
NikolaiZarudnev <nikolaizarudnrv@gmail.com>
2022-01-20 03:25:46 +0700

    HOW EDIT STATE???

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -1,7 +1,6 @@
 class StatesController < ApplicationController
     def index
-        @user = User.find(params[:user_id])
-        @state = @user.state.find(params[:id])
+        @states = State.all
     end
 
     def show
@@ -17,22 +16,34 @@ class StatesController < ApplicationController
         @state.happy = 0
         @state.tired = 0
         @state.money = 300
+        @state.save
     end
 
     def create
-        @state = State.find(params[:user_id])
-        @state = @user.states.create(state_params)
+        @user = User.find(params[:user_id])
+        @state = @user.state.create(params)
         redirect_to root_path
     end
-
-    def update; end #zdes menyat state ot action
+    def edit
+        @user = User.find(params[:user_id])
+        @state = @user.states.find(params[:id])
+    end
+    def update
+        @user = User.find(params[:user_id])
+        @state = @user.states.find(params[:id])
+    
+        @state.update(state_params)
+    
+        redirect_to user_path(@user)
+    end
+    
 
     def destroy; end
 
     private
 
     def state_params
-        params.require(:state).permit(:health, :alcohol, :happy, :tired, :money)
+        params.require(:state).permit(:user_id)
     end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'home/index'
+
   #get 'valera_actions/index'
   resource :valera_actions do
     resources :conditions
